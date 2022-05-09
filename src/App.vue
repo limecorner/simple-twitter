6fdf5ef56bf152bfc01f3e29c6f46393a31ed1e7
Limecorner <iversonwang40@gmail.com>
2022-05-09 14:21:03 +0800

    Bootstrap + normalize CSS

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
   <div id="app">
     <h1>Simple Twitter</h1>
+    <button type="button" class="btn btn-info">Info</button>
     <router-view />
   </div>
 </template>
+
+<script>
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+export default {
+  name: "App",
+};
+</script>
 
 
 
